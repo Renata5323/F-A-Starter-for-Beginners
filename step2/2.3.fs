bcf6bfcbc1b50b7936c575c4890ebf2c751afcf7
Renata5323 <125386628+Renata5323@users.noreply.github.com>
2026-09-10 17:05:48 +0300

    Create 2.3.fs

---
+++ step2/2.3.fs
@@ -0,0 +1,26 @@
+//Задание 1
+let age: int = 25
+
+//Задание 2
+let number: int16 = 1000s
+
+//Задание 3
+let number: int64 = 10000000000L
+
+//Задание 4
+let number: uint16 = 50000us
+
+//Задание 5
+let number: uint64 = 10000000000UL
+
+//Задание 6
+let price: float = 19.99
+
+//Задание 7
+let price: decimal = 19.99M
+
+//Задание 8
+let letter: char = 'A'
+
+//Задание 9
+let result: unit = ()
